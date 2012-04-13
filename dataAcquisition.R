21c6a92c65cf1ba63610492d30ac01249bb84528
erichhuang <erich.huang@sagebase.org>
2012-04-13 11:42:09 -0700

    Added commented out code to annotate custom feature names.

--- dataAcquisition.R
+++ dataAcquisition.R
@@ -3,6 +3,14 @@
 require(synapseClient)
 require(Biobase)
 require(affy)
+require(mg.hgu133a.db) # Brig Mecham's custom mappings for SCR workflow datasets
 
 ### BRING IN THE DATA FROM SAGE COMMONS REPOSITORY
 ovarianCaEnt <- loadEntity(273923)
+ovarianCaMat <- ovarianCaEnt$objects$normMat
+###
+
+### ANNOTATE SCR CUSTOM FEATURES TO GENE SYMBOLS
+# someProbes <- rownames(ovarianCaMat)[1:100]
+# hsym <- as.character(mg.hgu133aSYMBOL)
+# foo <- hsym[someProbes]
